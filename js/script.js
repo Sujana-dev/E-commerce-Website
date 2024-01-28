6648ca00d532f4f21d39833f389b643e697f9db2
Sujana-dev <sujanaa482@gmail.com>
2024-01-28 19:48:01 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+$('.banner_slide').slick({
+    slidesToShow: 1,
+    autoplay: true,
+    prevArrow:'<i class="fa-solid fa-chevron-right nextarr"></i>',
+    nextArrow:'<i class="fa-solid fa-chevron-left prevarr"></i>',
+    autoplaySpeed: 2000,
+    dots:true,
+  });
+
+  $('.deal_slider').slick({
+    slidesToShow: 1,
+    autoplay: false,
+    prevArrow:'<i class="fa-solid fa-chevron-right nextarr"></i>',
+    nextArrow:'<i class="fa-solid fa-chevron-left prevarr"></i>',
+    autoplaySpeed: 2000,
+  });
+
+  $('.bestSeller_slider').slick({
+    slidesToShow: 1,
+    autoplay: true,
+    prevArrow:'<i class="fa-solid fa-chevron-right nextarr"></i>',
+    nextArrow:'<i class="fa-solid fa-chevron-left prevarr"></i>',
+    autoplaySpeed: 2000,
+  });
